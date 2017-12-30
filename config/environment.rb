26b2285e23d3eb7f012011791921d1a40fd27909
jonathan.pirro <jonathan.pirro@wbgames.com>
2017-12-29 18:00:07 -0800

    changes for tgwtm

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ PAPERCLIP_ATTACHMENT_OPTIONS = {
   s3_protocol:    'https',
   s3_host_name:   "s3-#{ENV.fetch('AWS_REGION')}.amazonaws.com",
   s3_credentials: {
-    bucket: "audreypenven.#{Rails.env}",
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
